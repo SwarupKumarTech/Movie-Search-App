673b8a1fe78d07cc4d8ae59bf663e3fdc68839a2
RajKumarisnotmyname <aasiandme@gmail.com>
2025-11-05 18:57:42 +0530

    final updation

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ViewDetails from './components/ViewDetails/ViewDetails';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <HashRouter basename='/Movie-Search-App/'>
+ <BrowserRouter basename='/Movie-Search-App/'>
     <Routes>
       <Route path='/' element={<App />}/>
       <Route path='/view/:imdbID' element={<ViewDetails />} />
     </Routes>
- </HashRouter>
+ </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
